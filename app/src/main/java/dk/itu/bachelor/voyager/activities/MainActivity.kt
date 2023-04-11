efdd7b242028826c57509ebe10274d2223f3fb01
Una Mathiesen Langeland <unla@itu.dk>
2023-04-11 15:55:50 +0200

    Project setup

--- app/src/main/java/dk/itu/bachelor/voyager/activities/MainActivity.kt
+++ app/src/main/java/dk/itu/bachelor/voyager/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package dk.itu.bachelor.voyager
+package dk.itu.bachelor.voyager.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dk.itu.bachelor.voyager.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/dk/itu/bachelor/voyager/models/User.kt
@@ -0,0 +1,5 @@
+package dk.itu.bachelor.voyager.models
+
+data class User(
+
+)
