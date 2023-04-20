e5f17dc2a9072ed59a06dcf293d389f8fc7500f3
jard <jard@itu.dk>
2023-04-20 11:18:56 +0200

    Implement bottomNavigation

--- app/src/main/java/dk/itu/bachelor/voyager/activities/MainActivity.kt
+++ app/src/main/java/dk/itu/bachelor/voyager/activities/MainActivity.kt
@@ -2,7 +2,12 @@ package dk.itu.bachelor.voyager.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.view.get
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dk.itu.bachelor.voyager.R
 import dk.itu.bachelor.voyager.databinding.ActivityMainBinding
 
@@ -13,10 +18,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
-        /*val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
-        val navController = navHostFragment.navController*/
+       val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.exploreFragment -> navController.navigate(R.id.exploreFragment)
+                R.id.itinerariesFragment -> navController.navigate(R.id.itinerariesFragment)
+                R.id.planFragment -> navController.navigate(R.id.planFragment)
+            }
+            true
+        }
 
         setContentView(binding.root)
 
     }
+
+    /*override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        with(item.itemId) {
+        }
+        return super.onOptionsItemSelected(item)
+    }*/
 }
