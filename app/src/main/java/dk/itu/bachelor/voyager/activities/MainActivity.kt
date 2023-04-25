b475fd8ecafc0d610f9d53bc6a95972aeca0ed51
Una Mathiesen Langeland <unla@itu.dk>
2023-04-25 14:53:05 +0200

    Merged authentication and experiences

--- app/src/main/java/dk/itu/bachelor/voyager/activities/MainActivity.kt
+++ app/src/main/java/dk/itu/bachelor/voyager/activities/MainActivity.kt
@@ -1,10 +1,16 @@
 package dk.itu.bachelor.voyager.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 import dk.itu.bachelor.voyager.ExperiencesByListFragment
 import dk.itu.bachelor.voyager.MainFragment
 import dk.itu.bachelor.voyager.R
@@ -12,6 +18,9 @@ import dk.itu.bachelor.voyager.databinding.ActivityMainBinding
 import dk.itu.bachelor.voyager.models.VoyagerVM
 
 class MainActivity : AppCompatActivity() {
+
+    //Setting up authentication
+    private lateinit var auth: FirebaseAuth
 
     //View binding for MainActivity
     private lateinit var binding: ActivityMainBinding
@@ -27,7 +36,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        //Initialize FireBase Auth.
+        auth = FirebaseAuth.getInstance()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
 
@@ -66,5 +78,23 @@ class MainActivity : AppCompatActivity() {
         }
 
         setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (auth.currentUser == null)
+            startLoginActivity()
+        val user = auth.currentUser
+    }
+
+    private fun startLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        //finish()
+    }
+
+
+    private fun toast(text: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this, text, duration).show()
     }
 }
